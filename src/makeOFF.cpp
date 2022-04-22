2992515394fc617db18efaf9a6c36e11a08fd41c
Jérôme Houdayer <jerome.houdayer@polytechnique.org>
2022-04-22 15:29:02 +0200

    added memorization to makeOFF

--- src/makeOFF.cpp
+++ src/makeOFF.cpp
@@ -1,6 +1,7 @@
 /** \file makeOFF.cpp
   A standalone program to create simple shapes in OFF format.
 */
+#include <map>
 #include "arg_parse.hpp"
 #include "iotools.hpp"
 #include "mesh.hpp"
@@ -9,20 +10,19 @@ using namespace std;
 using namespace argparse;
 
 string sh =
-  "Creates or modifies a shape in OFF format.\n"
+  "Creates or modifies shapes in OFF format.\n"
   "Outputs it on standard output in OFF format.";
 string eh =
   "Operations are executed in the order of the command line.\n"
   "One can read or create multiple shapes into one.\n"
-  "If no file is given and no shapes created, then reads standard input.\n"
   "The shapes created by makeOFF have originally a radius equal to one.\n\n"
   "Examples:\n"
   "makeOFF --cube: creates a cube.\n"
-  "makeOFF -cr 1 file: reads shape from file, centers it and fixes its radius to 1.\n"
-  "makeOFF -v \"1 0 0\" file: translates the given form with the given vector.\n"
-  "makeOFF file1 file2 file3: creates a form containing the 3 forms given in files.\n"
+  "makeOFF -l file -cr 1 : reads shape from file, centers it and fixes its radius to 1.\n"
+  "makeOFF -l file -t \"1 0 0\": translates the given form with the given vector.\n"
+  "makeOFF -l file1 -l file2 -l file3: creates a form containing the 3 forms given in files.\n"
   "makeOFF --sphere -s4: creates a sphere with 5120 facets.\n";
-string FILES_help =  "reads files FILES in OFF format";
+string l_help =  "adds file FILE in OFF format to the current shape\n";
 string c_help = "centers the shape around the center of mass";
 string r_help = "rescales the shape to fix the radius to R";
 string s_help = "multiplies the number of facets by four N times,\n"
@@ -31,7 +31,7 @@ string t_help =
   "translates the shape along the given vector (3 numbers inside a quote)";
 string a_help =
   "rotates the shape with the given angle in degrees and axis,\n"
-  "4 numbers inside a quote (\"x y z angle\")";
+  "4 numbers inside a quote (\"x y z angle\")\n";
 string d_help =
   "applies a diagonal matrix (i.e. expands along the axis)\n"
   "the three expansion factors are given inside a quote";
@@ -45,24 +45,29 @@ string tet_help = "creates a regular tetrhedron with 4 facets";
 string dod_help = "creates a regular dodecahedron with 60 facets";
 string sph_help = "creates a sphere with 24 facets";
 string tor_help = "creates a torus with the given inner radius\n"
-                  "the number of facets increases with radius starting at 123";
+                  "the number of facets increases with radius starting at 123\n";
+string mem_help = "Memorizes the current shape under the given NAME";
+string rec_help = "Adds the shape memorized under NAME to the current shape";
+string clear_help = "Starts with a fresh empty shape\n";
 
 int main (int argc, char *argv[])
 {
   int digit = 6;
+  vector<string> fn;
   vector<double> tor_radius;
   vector<int> s;
   vector<double> r;
   vector<vec> t;
   vector<vec> d;
   vector<w_vec> a;
-  vector<string> fs;
   double project = 0;
   vector<opt_recorder> rec;
+  vector<string> mem;
+  vector<string> recall;
 
   parser p(sh, eh);
   p.prog_name = "makeOFF";
-  p.rest_arg("FILES", fs, FILES_help);
+  p.rec_list_option("l", "load", "FILE", fn, rec, l_help);
   p.rec_flag("", "cube", rec, cub_help);
   p.rec_flag("", "icosahedron", rec, ico_help);
   p.rec_flag("", "octahedron", rec, oct_help);
@@ -76,6 +81,9 @@ int main (int argc, char *argv[])
   p.rec_list_option("d", "", "FACTORS", d, rec, d_help);
   p.rec_list_option("t", "", "VEC", t, rec, t_help);
   p.rec_list_option("a", "", "VEC_ANGLE", a, rec, a_help);
+  p.rec_list_option("", "memorize", "NAME", mem, rec, mem_help);
+  p.rec_list_option("", "recall", "NAME", recall, rec, rec_help);
+  p.rec_flag("", "clear", rec, clear_help);
   p.option("f", "", "DIGITS", digit, f_help);
   p.option("e", "", "DIGITS", digit, e_help);
   p.flag("i", "", i_help);
@@ -87,32 +95,30 @@ int main (int argc, char *argv[])
   else if (p("e"))
     cout << scientific << setprecision(digit);
 
-
-  if (fs.empty() &&
-      p.none({"cube", "icosahedron", "octahedron", "tetrahedron",
-              "dodecahedron", "sphere", "torus"}))
-    fs.push_back("-");
-
+  map<string, mesh> memo;
   mesh m;
-  for (auto &filename: fs) {
-    string err = read_file(filename, m);
-    if (!err.empty())
-      p.die(err);
-  }
- 
+  
   for (auto opt: rec) {
     const string &n = opt.name;
-    if (n == "sphere") {
+
+    if (n == "l") {
+      mesh m0;
+      string err = read_file(fn[opt.pos], m0);
+      if (!err.empty())
+        p.die(err);
+      m.add(m0);
+    }
+    else if (n == "sphere") {
       project = (m.empty()) ? -1 : -2;
       m.add(make_icosahedron());
       continue;
     }
-    if (n == "torus") {
+    else if (n == "torus") {
       project = (m.empty()) ? tor_radius[opt.pos] : -2;
       m.add(make_torus(tor_radius[opt.pos]));
       continue;
     }
-    if (n == "s") {
+    else if (n == "s") {
       for (int i = 0 ; i < s[opt.pos] ; i++) {
         m = m.split();
         if (project == -1)
@@ -122,7 +128,7 @@ int main (int argc, char *argv[])
         }
       continue;
     }
-    project = -2; 
+    project = -2;
     if (n == "cube")
       m.add(make_cube());
     else if (n == "icosahedron")
@@ -133,7 +139,8 @@ int main (int argc, char *argv[])
       m.add(make_tetrahedron());
     else if (n == "dodecahedron")
       m.add(make_dodecahedron());
-    else if (n == "c")
+    
+    if (n == "c")
       m -= m.mass_center();
     else if (n == "r")
       m *= r[opt.pos] / m.radius();
@@ -143,7 +150,14 @@ int main (int argc, char *argv[])
       m.apply(rotation_mat(a[opt.pos].v.normalize(),
               a[opt.pos].weight * 3.141592653589793238 / 180));
     else if (n == "t")
-      m += t[opt.pos]; 
+      m += t[opt.pos];
+    
+    if (n == "memorize")
+      memo[mem[opt.pos]] = m;
+    else if (n == "recall")
+      m.add(memo[recall[opt.pos]]);
+    else if (n == "clear")
+      m = mesh();
   }
 
   if (p("i")) {
