325f598bd539671142da22bf64c950f719228a70
Jérôme Houdayer <jerome.houdayer@polytechnique.org>
2022-04-26 22:12:46 +0200

    added approx evaluation to rzm

--- src/rzm.cpp
+++ src/rzm.cpp
@@ -23,6 +23,8 @@ string e_help = "Numerical precision in scientific notation";
 string t_help = "Threshold value which separates the inside\n"
                 "from the outside (default is 1/2)";
 string r_help = "Does not regularized the mesh";
+string a_help = "Approximate density evaluation, give n, low, high in quote (e.g. \"30 0.2 0.8\")\n"
+                "Does only compute up to n if intermediate result is outside the given thresholds";
 string N_help = "The maximum order of Zernike moments to use (if available)";
 string RES_help = "Résolution of the mesh (i.e. number of intervals between -1 and 1)";
 string FILE_help = "Reads FILE in ZM format (default is standard input)";
@@ -35,13 +37,51 @@ int digit = 6;
 int res;
 double thresh = 0.5;
 
+class cascading_eval
+{
+public:
+  std::function<double(const vec &)> fast, slow;
+  const double t_low, t_high;
+
+  cascading_eval(std::function<double(const vec &)> f1, double tl, double hl, std::function<double(const vec &)> f2):
+  fast(f1), slow(f2), t_low(tl), t_high(hl)
+  {}
+
+  double operator() (const vec &v)
+  {
+    const double result = fast(v);
+    if (result < t_low || result > t_high)
+      return result;
+    else
+      return slow(v);
+  }
+};
+
+class approx_data
+{
+public:
+  int n;
+  double tl, hl;
+};
+
+istream &operator >>(istream &is, approx_data &ad)
+{
+  approx_data a;
+  is >> a.n >> a.tl >> a.hl;
+  if (is)
+    ad = a;
+  return is;
+}
+
 int main (int argc, char *argv[])
 {
   elapsed timer;
+  approx_data ad;
   string filename = "-";
   p.prog_name = "rzm";
 
   p.flag("v", "verbose", v_help);
+  p.option("a", "approx", "n low high", ad, a_help);
   p.option("f", "", "DIGITS", digit, f_help);
   p.option("e", "", "DIGITS", digit, e_help);
   p.flag("r", "raw", r_help);
@@ -75,8 +115,17 @@ int main (int argc, char *argv[])
   }
   zm.normalize(zm_norm::dual);
 
-  zernike_build f(zernike(N, zm));
-  cout << marching_tetrahedra({-1, 1, res}, {-1, 1, res}, {-1, 1, res}, f, thresh, !p("r"), p("v"));
+  zernike_eval f(zernike(N, zm));
+  mesh m;
+  if (p("a")) {
+    zernike_eval fast(zernike(ad.n, zm));
+    cascading_eval g(fast, ad.tl, ad.hl, f);
+    m = marching_tetrahedra({-1, 1, res}, {-1, 1, res}, {-1, 1, res}, g, thresh, !p("r"), p("v"));
+  }
+  else
+    m = marching_tetrahedra({-1, 1, res}, {-1, 1, res}, {-1, 1, res}, f, thresh, !p("r"), p("v"));
+  
+  cout << m;
   
   if (p("v"))
     cerr << "rzm used " << (int) (timer.seconds() * 100) / 100. << " seconds to run.\n"; 
