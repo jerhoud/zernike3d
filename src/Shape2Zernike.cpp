312d8eb6ab27dc5a3c978fa318ef4efe881cb381
Jérôme Houdayer <jerome.houdayer@polytechnique.org>
2022-10-18 15:02:33 +0200

    Cosmetic changes to Shape2Zernike

--- src/Shape2Zernike.cpp
+++ src/Shape2Zernike.cpp
@@ -25,7 +25,7 @@ string ex = "Shape2Zernike 50 shape.off                     Computes the Zernike
             "Shape2Zernike -a 8 -o result.zm 50 shape.off   Same using approximate algorithm with 8 digit precision and results written to file\n"
             "Shape2Zernike -vt 4 50 shape.off               Same running on four threads, with progression bar";
 string v_help = "outputs more informations, including progression bars";
-string o_help = "Save output to the given file instead of standard output";
+string o_help = "save output to the given file instead of standard output";
 string t_help = "number of threads to use in parallel, use 0 to adapt to the machine";
 string tests_help = "runs internal sanity checks and exits";
 string i_help = "computes Zernike rotational invariants instead of moments";
@@ -40,7 +40,7 @@ string d_help = "number of significant digits printed in the output (default is 
 string FILE_help = "reads FILE in OFF or ZM format (default is standard input)";
 string N_help = "the maximum order of Zernike moments computed";
 string die_N_msg ="N must be positive and no more than "
-                       + n_exact + " for exact computation of the moments.";
+                       + n_exact + " for exact computation of the moments";
 string radius_warning =
   "Warning: shape radius is larger than one. Risks of imprecisions.";
 string approx_warning = "Warning; requested precision is very small, program may not halt. Allowed error by facet: ";
@@ -49,6 +49,8 @@ string bad_output_msg = "Cannot open output file: ";
 
 int main (int argc, char *argv[])
 {
+  // Initialization
+
   elapsed timer;
   int N = 0;
   int digit = 8;
@@ -58,6 +60,7 @@ int main (int argc, char *argv[])
   string filename = "-";
   string output = "-";
   string zm_filename;
+
 
   // Set command line options 
 
@@ -82,6 +85,7 @@ int main (int argc, char *argv[])
   p.arg("N", N, N_help);
   p.opt_arg("FILE", filename, FILE_help);
 
+
   // Parse command line
 
   p.run(argc, argv);
@@ -90,7 +94,7 @@ int main (int argc, char *argv[])
   if (!out)
     p.die(bad_output_msg + output + " (" + strerror(errno) + ")");
 
-// Apply options -e and -f
+  // Apply options -a and -d
 
   const double approx_err = pow(0.1, approx);
   if (p("a") && !p("d"))
@@ -98,6 +102,7 @@ int main (int argc, char *argv[])
   if (digit <= 0)
     digit = 1;
   out << setprecision(digit);
+
 
   // Option --tests auto tests and exits
 
@@ -111,7 +116,15 @@ int main (int argc, char *argv[])
     return 0;
   }
 
+
+  // check N
+
+  if (N < 0)
+    p.die(die_N_msg);  
+
+
   // number of threads
+
   #ifdef NO_THREADS
     if (nt != 1)
       p.warn("Threads are not available in this build, running on one thread");
@@ -127,50 +140,52 @@ int main (int argc, char *argv[])
   }
   #endif
 
-  // 
 
-  if (N < 0)
-    p.die(die_N_msg);
-  zernike zm;
+  // Prepare input stream 
+  
   smart_input is(filename);
   if (!is)
     p.die(cannot_open_msg + is.name + " (" + strerror(errno) + ")");
+  
 
-  // Output header
+  // Write ouput header
 
   out << "# Produced by " << p.prog_name << " (" << p.version_text << ") from file: " << is.name << "\n";
   out << "# Date: " << now() << "\n";
 
+
   // Identify type of input file
   
+  zernike zm;
+
   istringstream iss;
   is.peek_line(iss);
   string filetype;
   iss >> filetype;
 
-  // it is a ZM file
+  // it is a ZM file, read it
   if (filetype == "ZM" || filetype == "zm") {
     zernike zm2;
     string err = read_object(is, zm2, p("v"));
     if (!err.empty())
       p.die(err);
     zm = zernike(N, zm2);
-    zm.output = zm_output::real;
   }
-  // it is an OFF file
+  // it is an OFF file, compute moments
   else if (filetype == "OFF" || filetype == "off") {
+    // check max bound on N
     if ((!p("a") && N > N_exact))
       p.die(die_N_msg);
+    // read OFF file
     mesh m;
     string err = read_object(is, m, p("v"));
     if (!err.empty())
       p.die(err);
-
+    // output some infos
     double rad = m.radius();
     out << "# Mesh: " << m.points.size() << " vertices, "
         << m.triangles.size() << " facets, "
         << "radius: " << rad << "\n";
-
     if (rad > 1.001)
       p.warn(radius_warning);
 
@@ -189,56 +204,38 @@ int main (int argc, char *argv[])
       zm = mesh_exact_integrate(m, N, triquad_schemes, nt, p("v"));
       out << "# error estimate: " << zm.get_error() << "\n";
     }
-
   }
+  // unknown file type
   else
     p.die(die_unknown_format + is.name);
   
-  // Select normalization and apply output options -c -p
+
+  // Select normalization and apply output options -r -p and -n
 
   zm.normalize(make_norm(false, false, p("n")));
   zm.output = make_output(!p("r"), p("p"));
 
   // option -d read a secondary zm file
 
-  zernike zm2;
   if (p("diff")) {
+    zernike zm2;
     string err = read_file(zm_filename, zm2, p("v"));
     if (!err.empty())
       p.die(err);
     zm2.normalize(zm_norm::ortho);
     out << "# Substracted data from file " << zm_filename << "\n";
+    zm = zm - zm2;
   }
 
-  // command -m output moments
-  if (p.none({"i", "s", "tests"})) {
-    if (p("diff"))
-      zm = zm - zm2;
-    out << zm;
-  }
-  // command -i output rotational invariants
-  else if (p("i")) {
-    rotational_invariants ri(N);
-    ri.eval_ri(zm);
-    if (p("diff")) {
-      rotational_invariants ri2(N);
-      ri2.eval_ri(zm2);
-      ri = ri - ri2;
-    }
-    out << ri;
-  }
-  // command -s output signature
-  else if (p("s")) {
-    signature_invariants si(N);
-    si.eval_si(zm);
-    if (p("diff")) {
-      signature_invariants si2(N);
-      si2.eval_si(zm2);
-      si = si - si2;
-    }
-    out << si;
-  }
+
+  // write result
+
+  out << zm;
+
+  // ciao !
 
   if (p("v"))
     cerr << p.prog_name << " used " << (int) (timer.seconds() * 100) / 100. << " seconds to run.\n"; 
+
+  return 0;
 }
