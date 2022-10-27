daf9a8ea135c005b25c5aa52ad67e018bfdb6059
Jérôme Houdayer <jerome.houdayer@polytechnique.org>
2022-10-27 16:18:41 +0200

    rewritten neldermead

--- src/geom/neldermead.cpp
+++ src/geom/neldermead.cpp
@@ -13,92 +13,81 @@ class vecval
 public:
   vec v;
   double val;
+  vecval(const vec &vc, std::function<double(const vec &)> f):
+  v(vc), val(f(vc)) {}
+  void set(const vec &vc, std::function<double(const vec &)> f)
+  { v = vc; val = f(vc); }
 };
 
-void sort4(vecval &v1, vecval &v2, vecval &v3, vecval &v4)
+void sort4(vecval *&v1, vecval *&v2, vecval *&v3, vecval *&v4)
 {
-  if(v1.val>v2.val) swap(v1, v2);
-  if(v3.val>v4.val) swap(v3, v4);
-  if(v1.val>v3.val) swap(v1, v3);
-  if(v2.val>v4.val) swap(v2, v4);
-  if(v2.val>v3.val) swap(v2, v3);
+  if(v1->val > v2->val) swap(v1, v2);
+  if(v3->val > v4->val) swap(v3, v4);
+  if(v1->val > v3->val) swap(v1, v3);
+  if(v2->val > v4->val) swap(v2, v4);
+  if(v2->val > v3->val) swap(v2, v3);
+}
+
+int insert4(vecval *&v1, vecval *&v2, vecval *&v3, vecval *&v4, const vecval &nv)
+{
+  const double v = nv.val;
+  if (v >= v4->val)
+    return 5;
+  *v4 = nv;
+  if (v >= v3->val)
+    return 4;
+  vecval *tmp = v4;
+  v4 = v3;
+  if (v >= v2->val) {
+    v3 = tmp;
+    return 3;
+  }
+  v3 = v2;
+  if (v >= v1->val) {
+    v2 = tmp;
+    return 2;
+  }
+  v2 = v1;
+  v1 = tmp;
+  return 1;
 }
 
 vec minimize(std::function<double(const vec &)> f, const vec &start, double scale, double fthresh, double vthresh, int itermax)
 {
   vthresh *= vthresh;
   
-  vecval v1{start, f(start)};
-  vecval v2{start + vec{scale, 0, 0}, f(start + vec{scale, 0, 0})};
-  vecval v3{start + vec{0, scale, 0}, f(start + vec{0, scale, 0})};
-  vecval v4{start + vec{0, 0, scale}, f(start + vec{0, 0, scale})};
+  vecval va(start, f);
+  vecval vb(start + vec{scale, 0, 0}, f);
+  vecval vc(start + vec{0, scale, 0}, f);
+  vecval vd(start + vec{0, 0, scale}, f);
 
+  vecval *v1 = &va, *v2 = &vb, *v3 = &vc, *v4 = &vd;
   sort4(v1, v2, v3, v4);
 
-  while (itermax-- != 0 && (v1.v - v4.v).length_square() > vthresh && fabs(v1.val - v4.val) > fthresh) {
-    // new points
-    vec b = (v1.v + v2.v + v3.v) / 3;
-    vec d = b - v4.v;
-    vec r = b + d;
-    vecval vr{r, f(r)};
-    if (vr.val < v1.val) { // expansion
-      v4 = v3;
-      v3 = v2;
-      v2 = v1;
-      vec e = r + d;
-      vecval ve{e, f(e)};
-      if (ve.val < v1.val)
-        v1 = ve;
+  while (itermax-- != 0 && (v1->v - v4->v).length_square() > vthresh && fabs(v1->val - v4->val) > fthresh) {
+    vec b = (v1->v + v2->v + v3->v) / 3;
+    vec d = b - v4->v;
+
+    const int p = insert4(v1, v2, v3, v4, vecval(b + d, f)); // reflection
+    if (p == 1) { // expansion
+      vecval ve(v1->v + d, f);
+      if (ve.val < v1->val)
+        *v1 = ve;
+    }
+    else if (p >= 4) { // contraction
+      d /= 2;
+      if (p == 4)
+        b += d;
       else
-        v1 = vr;
-      continue;
-    }
-    if (vr.val < v3.val) { // reflection
-      v4 = v3;
-      if (vr.val < v2.val) {
-        v3 = v2;
-        v2 = vr;
+        b -= d;
+      const int p2 = insert4(v1, v2, v3, v4, vecval(b, f));
+      if (p2 == 5) { // shrink
+        v2->set((v2->v + v1->v) / 2, f);
+        v3->set((v3->v + v1->v) / 2, f);
+        v4->set((v4->v + v1->v) / 2, f);
+        sort4(v1, v2, v3, v4);
       }
-      else
-        v3 = vr;
-      continue;
-    }
-    d /= 2;
-    vec c;
-    if (vr.val < v4.val) { // outer contraction
-      c = b + d;
-      v4 = vr;
-    }
-    else // inner contraction
-      c = b - d;
-    vecval vc{c, f(c)};
-    if (vc.val < v4.val) { // contraction
-      if (vc.val >= v3.val)
-        v4 = vc;
-      else {
-        v4 = v3;
-        if (vc.val >= v2.val)
-          v3 = vc;
-        else {
-          v3 = v2;
-          if (vc.val >= v1.val)
-            v2 = vc;
-          else {
-            v2 = v1;
-            v1 = vc;
-          }
-        }
-      }
-      continue;
-    }
-    else { // shrink
-      v2.v += (v2.v - v1.v) / 2;
-      v2.val = f(v2.v);
-      v3.v += (v3.v - v1.v) / 2;
-      v3.val = f(v3.v);
-      v4.v += (v4.v - v1.v) / 2;
-      v4.val = f(v4.v);
     }
   }
-  return v1.v;
+  return v1->v;
 }
